9f14b7f4c2d6e9e2c4ca90b13c4e78b6c0226788
olivia <opierez@gmail.com>
2022-11-21 18:00:26 -0500

    commit tree b892cb1e35ede4ba6d699c112428e849da794f0b

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(pickupLocation - 42);
+}
+
+// function distanceFromHqInFeet(pickupLocation) {
+//     return distanceFromHqInBlocks(pickupLocation) * 264;
+// }
+
+function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+
+}
+
+function calculatesFarePrice(start, destination) {
+    let distanceTravelled = distanceTravelledInFeet(start, destination);
+
+
+    if (distanceTravelled <= 400) {
+        return 0;
+    } else if (distanceTravelled > 400 && distanceTravelled <= 2000) {
+        return (distanceTravelled - 400) * .02
+    } else if (distanceTravelled > 2000 && distanceTravelled < 2500) {
+        return 25;
+    } else if (distanceTravelled > 2500) {
+        return `cannot travel that far`;
+    }
+}
+
+console.log(distanceFromHqInBlocks(43));
+console.log(distanceFromHqInBlocks(50));
+console.log(distanceFromHqInFeet(43));
+console.log(distanceFromHqInFeet(50));
+console.log(distanceFromHqInFeet(34));
+console.log(distanceTravelledInFeet(43, 48));
+console.log(distanceTravelledInFeet(50, 60));
+console.log(distanceTravelledInFeet(34, 28));
